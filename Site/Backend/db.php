f7222e6d9de904f9b03d84d4dbeb0cea8b1cde9c
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-08 15:03:34 +0100

    Funzioni get_book_by_isbn, get_book_reviews, get_books_by_tag, login

--- Site/Backend/db.php
+++ Site/Backend/db.php
@@ -27,4 +27,77 @@ class Service extends Constant {
   public function closeConnection(): void {
     $this->connection->close();
   }
+
+  public function get_book_by_isbn($isbn): array {
+    $query = "SELECT * FROM libro INNER JOIN pubblicazione ON pubblicazione.libro_isbn = libro.isbn INNER JOIN autore ON autore.id = pubblicazione-autore_id WHERE libro.isbn = ?";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param('s', $isbn);
+    $tmp = $stmt->execute();
+
+    if ($tmp->num_rows == 0) {
+      return NULL;
+    }
+
+    $result = array();
+
+    while ($row = $tmp->fetch_assoc()) {
+      array_push($result, $row);
+    }
+
+    $tmp->free();
+    $stmt->close();
+    return $result;
+  }
+
+  public function get_book_reviews($isbn): array {
+    $query = "SELECT * FROM recensione WHERE libro_isbn = ?";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param('s', $isbn);
+    $tmp = $stmt->execute();
+
+    if ($tmp->num_rows == 0) {
+      return NULL;
+    }
+
+    $result = array();
+
+    while ($row = $tmp->fetch_assoc()) {
+      array_push($resul, $row);
+    }
+
+    $tmp->free();
+    $stmt->close();
+    return $result;
+  }
+
+  public function get_books_by_tag($tag): array {
+    $query = "SELECT * FROM libro INNER JOIN tipologia ON tipologia.libro_isbn = libro.isbn WHERE tipologia.tag_id = ?";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param('i', $tag);
+    $tmp = $stmt->execute();
+
+    if ($tmp->num_rows == 0) {
+      return NULL;
+    }
+
+    $result = array();
+
+    while ($row = $tmp->fetch_assoc()) {
+      array_push($result, $row);
+    }
+
+    $tmp->free();
+    $stmt->close();
+    return $result;
+  }
+
+  public function login($mail, $password): bool {
+    $query = "SELECT * FROM utente WHERE mail = ? AND password = ?";
+    $stmt = $this->connection->prepare($query);
+    $hash_password = hash('sha256', $password);
+    $stmt->bind_param('ss', $mail, $hash_password);
+    $tmp = $stmt->execute();
+
+    return $tmp->num_rows;
+  }
 }
