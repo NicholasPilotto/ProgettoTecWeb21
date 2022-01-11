ce4f22a069766b40d74c8a94ddfb77b4815732fe
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-11 19:41:05 +0100

    Miglioria funzione

--- Site/frontend/graphics.php
+++ Site/frontend/graphics.php
@@ -1,14 +1,12 @@
 <?php
-    class graphics 
-    {
-        public static function getPage($nome)
-        {
-            $headerHTML = file_get_contents("header.html");
-            $paginaHTML = file_get_contents($nome);
-            $footerHTML = file_get_contents("footer.html");
+class graphics {
+    public static function getPage($nome, $root = false) {
+        $aux = $root ? "frontend/" : "";
+        $headerHTML = file_get_contents($root . "header.html");
+        $paginaHTML = file_get_contents($nome);
+        $footerHTML = file_get_contents($root . "footer.html");
 
-            $paginaHTML = str_replace("<headerSito/>", $headerHTML, $paginaHTML);
-            return str_replace("<footerSito/>", $footerHTML, $paginaHTML);
-        }
+        $paginaHTML = str_replace("<headerSito/>", $headerHTML, $paginaHTML);
+        return str_replace("<footerSito/>", $footerHTML, $paginaHTML);
     }
-?>
+}
--- Site/index.php
+++ Site/index.php
@@ -1,10 +1,10 @@
 <?php
-    require_once "graphics.php";
-    
-    $paginaHTML = graphics::getPage("views/index_php.html");
+require_once "graphics.php";
 
-    // Accesso al database
+$paginaHTML = graphics::getPage("views/index_php.html", true);
 
-    // -------------------
+// Accesso al database
 
-    echo $paginaHTML;
+// -------------------
+
+echo $paginaHTML;
