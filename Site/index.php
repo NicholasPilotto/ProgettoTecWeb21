c6af1a88ecc0b1ee2979726fc19e667890d4d95c
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-11 19:09:12 +0100

    Suddivisione della parte frontend

--- Site/index.php
+++ Site/index.php
@@ -1,11 +1,10 @@
 <?php
     require_once "graphics.php";
     
-    $paginaHTML = graphics::getPage("index_php.html");
+    $paginaHTML = graphics::getPage("views/index_php.html");
 
     // Accesso al database
 
     // -------------------
 
     echo $paginaHTML;
-?>
