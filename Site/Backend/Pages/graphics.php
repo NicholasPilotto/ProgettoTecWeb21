5e6f562cf05dd86913da38a5ccaf9358189e83ac
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-11 14:47:42 +0100

    Correzione path

--- Site/Backend/Pages/graphics.php
+++ Site/Backend/Pages/graphics.php
@@ -1,9 +1,9 @@
 <?php
 class graphics {
     public static function getPage($nome) {
-        $headerHTML = file_get_contents("../../Views/header.html");
+        $headerHTML = file_get_contents("./Views/header.html");
         $paginaHTML = file_get_contents($nome);
-        $footerHTML = file_get_contents("../../Views/footer.html");
+        $footerHTML = file_get_contents("./Views/footer.html");
 
         $paginaHTML = str_replace("</headerSito>", $headerHTML, $paginaHTML);
         return str_replace("</footerSito>", $footerHTML, $paginaHTML);
