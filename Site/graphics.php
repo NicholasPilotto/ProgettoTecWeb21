537b1bda3b0a9539e2eff6a58ea17612dd775066
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2022-01-12 19:15:54 +0100

    Aggiunta </link> e nuovo style

--- Site/graphics.php
+++ Site/graphics.php
@@ -4,10 +4,12 @@
         public static function getPage($nome)
         {
             $headerHTML = file_get_contents("header.html");
+            $linkHTML = file_get_contents("link.html");
             $paginaHTML = file_get_contents($nome);
             $footerHTML = file_get_contents("footer.html");
 
             $paginaHTML = str_replace("</headerSito>", $headerHTML, $paginaHTML);
+            $paginaHTML = str_replace("</linkSito>", $linkHTML, $paginaHTML);
             return str_replace("</footerSito>", $footerHTML, $paginaHTML);
         }
     }
