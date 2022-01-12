8a156560150cbc48902bf6671dac573a8ff08cf1
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2022-01-12 19:26:00 +0100

    aggiunta file registrazione (da fare)

---
+++ Site/registrati.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "graphics.php";
+    
+    $paginaHTML = graphics::getPage("registrati_php.html");
+
+    // Accesso al database
+
+    // -------------------
+
+    echo $paginaHTML;
+?>
