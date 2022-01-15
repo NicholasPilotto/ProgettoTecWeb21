e5f58cccdbd86001b40ec3de0e76d3ec9b9754c8
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-15 14:26:38 +0100

    Query per la ricerca di libri tramite autore

--- Site/backend/db.php
+++ Site/backend/db.php
@@ -88,4 +88,36 @@ class Service extends Constant {
     $stmt->close();
     return $result;
   }
+
+  public function get_books_by_author($author_firstname, $author_lastname): array {
+    $query = "SELECT * FROM libro INNER JOIN pubblicazione ON pubblicazione.libro_isbn = libro.isbn INNER JOIN autore ON autore.id = pubblicazione.autore_id INNER JOIN editore ON libro.editore = editore.id WHERE autore.nome LIKE ? AND autore.cognome LIKE ?";
+    $stmt = $this->connection->prepare($query);
+    $result = array();
+
+    if ($stmt === false) {
+      return $result;
+    }
+
+    $first = '%' . $author_firstname . '%';
+    $last = '%' . $author_lastname . '%';
+
+
+    if ($stmt->bind_param('ss', $first, $last) === false) {
+      return $result;
+    }
+
+    $stmt->execute();
+    $tmp = $stmt->get_result();
+
+    if ($tmp->num_rows == 0) {
+      return $result;
+    }
+
+    while ($row = $tmp->fetch_assoc()) {
+      array_push($result, $row);
+    }
+
+    $stmt->close();
+    return $result;
+  }
 }
--- Site/backend/test.php
+++ Site/backend/test.php
@@ -2,26 +2,13 @@
 
 use DB\Service;
 
+//path /Site/test.php?f=a
+
 require_once('db.php');
 if ($_GET['f'] == 'a') {
   $connessione = new Service();
   $a = $connessione->openConnection();
-  $aux = $connessione->get_book_by_title('La canzone romana');
+  $aux = $connessione->get_books_by_author('Elen', 'Bonelli');
   print_r(json_encode($aux));
-  // $connessione->closeConnection();
-
-
-  // $a = new mysqli("127.0.0.1", "root", "", "tecweb");
-
-  // if ($a->connect_error) {
-  //   echo "Connection failed: " . $a->connect_error;
-  // }
-
-  // $r = $a->query("SELECT * FROM personaggi ORDER BY id ASC");
-
-  // while ($row = $r->fetch_assoc()) {
-  //   echo $row['nome'];
-  // }
-
-  // mysqli_close($a);
+  $connessione->closeConnection();
 }
