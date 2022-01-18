6ce63d387b14618090bf177f0e2fb91b89654e59
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2022-01-18 19:07:40 +0100

    Add files via upload

--- Site/bestseller.php
+++ Site/bestseller.php
@@ -1,10 +1,33 @@
 <?php
+    use DB\Service;
+    require_once('backend/db.php');
+
     require_once "graphics.php";
     
     $paginaHTML = graphics::getPage("bestseller_php.html");
 
     // Accesso al database
+    $connessione = new Service();
+    $a = $connessione->openConnection();
 
+    $limit = 12;
+
+    $queryBestseller = $connessione->get_bestsellers();
+
+    $listaBestseller = "<ul class='bookCards'>";
+    $cont = 0;
+    foreach($queryBestseller as $libro)
+    {
+        if($cont++ < $limit)
+        {
+            $listaBestseller .= "<li><a href=''><img class='generiCardsImg' src='" . $libro['Percorso'] ."' alt=''>" . $libro['Titolo'] . "</a></li>";
+        }
+    }
+    $listaBestseller .= "</ul>";
+
+    $paginaHTML = str_replace("</listaBestseller>", $listaBestseller, $paginaHTML);
+
+    $connessione->closeConnection();
     // -------------------
 
     echo $paginaHTML;
