2cf04ac802b71fa82e6782e25c47ab49bf612d7c
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-21 16:32:37 +0100

    Nuovi metodi alla classe wrapper

--- Site/backend/response_manager.php
+++ Site/backend/response_manager.php
@@ -3,26 +3,36 @@
 namespace DB;
 
 class response_manager {
+
   private $empty = false;
   private $result = array();
-  private $errno = NULL;
-  private $error_message = NULL;
+  private $errno = 0;
+  private $error_message_mysqli = "";
+  private $error_message = "";
 
-  public function __construct($res, $conn) {
-    if (isset($res)) {
-      $this->result = $res;
-      $this->empty = false;
+  public function __construct($res, $conn, $mess) {
+    $this->result = $res;
+
+    $this->error_message = $mess;
+    $this->empty = count($res) != 0 ? false : true;
+
+    if ($conn) {
+      $this->error_message_mysqli = $conn->error;
+
+      $this->errno = $conn->errno;
     }
-
-    $this->errno = $conn->error;
   }
 
-  public function set_message($mes): void {
-    $this->error_message = $mes;
+  public function ok(): bool {
+    return !$this->empty && !$this->errno;
   }
 
   public function is_empty(): bool {
     return $this->empty;
+  }
+
+  public function get_element_count(): int {
+    return count($this->result);
   }
 
   public function get_result(): array {
@@ -33,8 +43,15 @@ class response_manager {
     return $this->error_message;
   }
 
+  public function get_error_message_mysqli(): string {
+    return $this->error_message_mysqli;
+  }
+
   public function get_errno(): int {
     return $this->errno;
   }
+
+  public function set_error_message($mes): void {
+    $this->error_message = $mes;
+  }
 }
-?>
