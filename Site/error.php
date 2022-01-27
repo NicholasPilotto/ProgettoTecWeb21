5ec6ddbb0e3504fcfed0b8a661a0253b684e2f32
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-27 14:51:01 +0100

    Controllo errore 404 e redirect pagina errore

---
+++ Site/error.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+use DB\Service;
+
+require_once "graphics.php";
+
+$paginaHTML = graphics::getPage("error_php.html");
+
+$errore = "<img src='images/404.jpg' alt='Errore 404, genere inesistente' id='erroreImg'>";
+
+$paginaHTML = str_replace("</errore>", $errore, $paginaHTML);
+
+echo $paginaHTML;
