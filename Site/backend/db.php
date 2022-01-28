e933d01285b5cdbbdd39f7c6f4985487c7346e90
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-01-28 23:34:34 +0100

    Se già presente un utente nella tabella Resoconto, viene aggiornato l'id

--- Site/backend/db.php
+++ Site/backend/db.php
@@ -9,7 +9,7 @@ require_once('response_manager.php');
 class Constant {
   protected const HOST_DB = "127.0.0.1";
   protected const DATABASE_NAME = "secondread";
-  protected const USERNAME = "root";
+  protected const USERNAME = "";
   protected const PASSWORD = "";
 }
 
@@ -863,8 +863,7 @@ class Service extends Constant {
     return $res;
   }
 
-  public function get_books_with_offers() : response_manager
-  {
+  public function get_books_with_offers(): response_manager {
     $query = "SELECT * FROM libro
               INNER JOIN offerte
               ON offerte.libro_ISBN = libro.ISBN
@@ -918,4 +917,101 @@ class Service extends Constant {
     return $res;
   }
 
+  public function restore_code($utente): response_manager {
+    $query = "INSERT INTO Recupero (id, utente) VALUES (?,?)
+              ON DUPLICATE KEY UPDATE 
+              id=?";
+    $id = md5(uniqid(rand(), true));
+    $stmt = $this->connection->prepare($query);
+    $result = array();
+
+    if ($stmt === false || $stmt->bind_param('sss', $id, $utente, $id) === false) {
+      return new response_manager($result, $this->connection, "Qualcosa sembra essere andato storto");
+    }
+
+    $stmt->execute();
+
+    array_push($result, $id);
+
+    $res = new response_manager($result, $this->connection, "");
+
+    if (!$res->ok()) {
+      $res->set_error_message("Non è stato possibile generare il codice");
+    }
+
+    $stmt->close();
+
+    return $res;
+  }
+
+  public function is_code_correct($id, $utente): response_manager {
+    $query = "SELECT *
+              FROM Recupero
+              WHERE id = ? AND utente = ?";
+    $stmt = $this->connection->prepare($query);
+    $result = array();
+
+    if ($stmt === false || $stmt->bind_param('ss', $id, $utente) === false) {
+      return new response_manager($result, $this->connection, "Qualcosa sembra essere andato storto");
+    }
+
+    $stmt->execute();
+    $tmp = $stmt->get_result();
+
+    while ($row = $tmp->fetch_assoc()) {
+      array_push($result, $row);
+    }
+
+    $res = new response_manager($result, $this->connection, "");
+
+    if (!$res->ok()) {
+      $res->set_error_message("Il codice non corrisponde");
+    }
+
+    $stmt->close();
+
+    return $res;
+  }
+
+  public function restore_psw($utente, $pass, $code): response_manager {
+    $query = "UPDATE utente SET password = ?";
+    $psw = hash('sha256', $pass);
+
+    $stmt = $this->connection->prepare($query);
+
+    $result = array();
+
+    if ($stmt === false || $stmt->bind_param('s', $psw) === false) {
+      return new response_manager($result, $this->connection, "Qualcosa sembra essere andato storto");
+    }
+
+    $response = $stmt->execute();
+
+    $stmt->close();
+
+    if (!$response) {
+      return new response_manager(array(), $this->connection, "Qualcosa sembra essere andato storto");
+    }
+
+    $query2 = "DELETE recupero WHERE id = ? AND utente = ?";
+    $stmt = $this->connection->prepare($query2);
+
+    if ($stmt === false || $stmt->bind_param('ss', $code, $utente) === false) {
+      return new response_manager($result, $this->connection, "Qualcosa sembra essere andato storto");
+    }
+
+    $stmt->execute();
+    $tmp = $stmt->get_result();
+    array_push($result, $tmp);
+
+    $res = new response_manager($result, $this->connection, "");
+
+    if (!$res->ok()) {
+      $res->set_error_message("Non è stato possibile generare il codice");
+    }
+
+    $stmt->close();
+
+    return $res;
+  }
 }
