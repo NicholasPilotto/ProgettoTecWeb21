7515dac26d9834987945f573483ef62ef7595133
casYy <51257666+casYy@users.noreply.github.com>
2022-01-31 10:39:18 +0100

    Add files via upload

--- Site/registrati.php
+++ Site/registrati.php
@@ -1,23 +1,30 @@
 <?php
-session_start();
-if (isset($_SESSION["Nome"])) {
-    header("Location: index.php");
-} else {
-    if (isset($_SESSION["error"])) {
-        $errorMsg = $_SESSION["error"];
-        session_destroy();
-    } else {
+    session_start();
+    if(isset($_SESSION["Nome"]))
+    {
+        header("Location: index.php");
+    }
+    else
+    {    
+        if(isset($_SESSION["error"]))
+        {
+            $errorMsg = "<h3>" . $_SESSION["error"] . "</h3>";
+            session_destroy();
+        }
+    else
+    {
         $errorMsg = "";
     }
-    require_once "graphics.php";
+        require_once "graphics.php";
 
-    $paginaHTML = graphics::getPage("registrati_php.html");
+        $paginaHTML = graphics::getPage("registrati_php.html");
 
-    // Accesso al database
+        // Accesso al database
 
-    // -------------------
+        // -------------------
 
-    $paginaHTML = str_replace("</errorMsg>", $errorMsg, $paginaHTML);
+        $paginaHTML = str_replace("</errorMsg>", $errorMsg, $paginaHTML);
 
-    echo $paginaHTML;
-}
+        echo $paginaHTML;
+    }
+?>
