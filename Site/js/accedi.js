ef64639d7587bc6400731cc21acdd756c5bb2f0b
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-04-14 11:28:48 +0200

    Migliorie js

--- Site/js/accedi.js
+++ Site/js/accedi.js
@@ -1,24 +1,4 @@
-let dettagli_form = {
-  "nome": [
-    "Nome",
-    /^[A-Za-zàèùìòé\s]\w{2,20}$/,
-    "Inserire un nome di lunghezza compresa tra i 2 e 20 caratteri"
-  ],
-  "cognome": [
-    "Cognome",
-    /^[A-Za-zàèùìòé\s]\w{2,20}$/,
-    "Inserire un cognome di lunghezza compresa tra i 2 e 20 caratteri"
-  ],
-  "telefono": [
-    "Numero telefonico",
-    /^[0-9]{10}$/,
-    "Inserire un numero telefonico corretto"
-  ],
-  "username": [
-    "Username",
-    /^[A-Za-z\s]\w{2,10}$$/,
-    "Inserire un username di lunghezza tra i 2 e 10 caratteri"
-  ],
+var dettagli_form = {
   "email": [
     "Indirizzo mail",
     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
@@ -27,12 +7,7 @@ let dettagli_form = {
   "password": [
     "Password",
     /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/,
-    "Inserire una password di almeno 8 caratteri, di cui: uno minuscolo, uno maiuscolo, un numero ed un carattere speciale"
-  ],
-  "conferma": [
-    "Password",
-    /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/,
-    "Le due password non coincidono"
+    "I valori inseriti non sembrano corrretti"
   ]
 };
 
@@ -60,14 +35,12 @@ function caricamento() {
 
 function campoDefault(input) {
   if (input.value == "") {
-    input.classname = "";
     input.value = dettagli_form[input.id][0];
   }
 }
 
 function campoPerInput(input) {
   if (input.value == dettagli_form[input.id][0]) {
-    input.classname = "";
     input.value = "";
   }
 }
@@ -83,30 +56,14 @@ function validazioneCampo(input) {
     mostraErrore(input);
     return false;
   }
-
-  if (input.id == "conferma" && !confirmPass()) {
-    mostraErrore(input);
-    return false;
-  }
-
   return true;
 }
 
-function confirmPass() {
-  const password = document.querySelector('input[name=password]');
-  const confirm = document.querySelector('input[name=conferma]');
-
-  if (confirm.value === password.value) {
-    return true
-  }
-  return false;
-}
-
-function mostraErrore(input) {
+function mostraErrore(input, increment = 0) {
   var padre = input.parentNode;
   var errore = document.createElement("strong");
   errore.className = "errorSuggestion";
-  errore.appendChild(document.createTextNode(dettagli_form[input.id][2]));
+  errore.appendChild(document.createTextNode(dettagli_form[input.id][2 + increment]));
   padre.appendChild(errore);
 }
 
@@ -114,10 +71,6 @@ function validazioneForm() {
   for (var key in dettagli_form) {
     var input = document.getElementById(key);
     if (!validazioneCampo(input)) {
-      return false;
-    }
-    if (input.id == "conferma" && !confirmPass(input)) {
-      mostraErrore(input);
       return false;
     }
   }
