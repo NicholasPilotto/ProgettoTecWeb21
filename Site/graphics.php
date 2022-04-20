5d9d4d61c3a23e1dcb4e867a72a5f4a3a1e5f8ce
Nicholas Pilotto <nicholaspilotto@gmail.com>
2022-04-20 13:05:19 +0200

    footer

--- Site/graphics.php
+++ Site/graphics.php
@@ -1,41 +1,37 @@
 <?php
-    class graphics 
-    {
-        public static function getPage($nome)
-        {
-            $headerHTML = file_get_contents("header.html");
-            
-            $linkUtente = "";
+class graphics {
+    public static function getPage($nome) {
+        $headerHTML = file_get_contents("header.html");
 
-            if(isset($_SESSION["Nome"]))
-            {
-                $linkUtente .= "<p id='benvenuto'>Benvenuto, " . $_SESSION["Nome"] . "</p>";
-                $linkUtente .= "<a class='linkUtente' href='account.php'>Account</a>";
-                $linkUtente .= "<a class='linkUtente' href=''>Carrello</a>";
-                $linkUtente .= "<a class='linkUtente' href='esci.php'>Esci</a>";
-            }
-            else
-            {           
-                $linkUtente .= "<a class='linkUtente' href='accedi.php'>Accedi</a>";
-                $linkUtente .= "<a class='linkUtente' href='registrati.php'>Registrati</a>";
-                $linkUtente .= "<a class='linkUtente' href=''>Carrello</a>";
-            }
-            $headerHTML = str_replace("</linkUtente>", $linkUtente, $headerHTML);
-            
-            /*
+        $linkUtente = "";
+
+        if (isset($_SESSION["Nome"])) {
+            $linkUtente .= "<p id='benvenuto'>Benvenuto, " . $_SESSION["Nome"] . "</p>";
+            $linkUtente .= "<a class='linkUtente' href='account.php'>Account</a>";
+            $linkUtente .= "<a class='linkUtente' href=''>Carrello</a>";
+            $linkUtente .= "<a class='linkUtente' href='esci.php'>Esci</a>";
+        } else {
+            $linkUtente .= "<a class='linkUtente' href='accedi.php'>Accedi</a>";
+            $linkUtente .= "<a class='linkUtente' href='registrati.php'>Registrati</a>";
+            $linkUtente .= "<a class='linkUtente' href=''>Carrello</a>";
+        }
+        $headerHTML = str_replace("</linkUtente>", $linkUtente, $headerHTML);
+
+        /*
             <a id="accedi" href="accedi.php">Accedi</a>
             <a id="registrati" href="registrati.php">Registrati</a>
             <a id="carrello" href="">Carrello</a>
             */
 
 
-            $linkHTML = file_get_contents("link.html");
-            $paginaHTML = file_get_contents($nome);
-            $footerHTML = file_get_contents("footer.html");
+        $linkHTML = file_get_contents("link.html");
+        $paginaHTML = file_get_contents($nome);
+        $footerHTML = file_get_contents("footer.html");
+        $thisYear = date("Y");
+        $footerHTML = str_replace("</currentDate>", $thisYear, $footerHTML);
 
-            $paginaHTML = str_replace("</headerSito>", $headerHTML, $paginaHTML);
-            $paginaHTML = str_replace("</linkSito>", $linkHTML, $paginaHTML);
-            return str_replace("</footerSito>", $footerHTML, $paginaHTML);
-        }
+        $paginaHTML = str_replace("</headerSito>", $headerHTML, $paginaHTML);
+        $paginaHTML = str_replace("</linkSito>", $linkHTML, $paginaHTML);
+        return str_replace("</footerSito>", $footerHTML, $paginaHTML);
     }
-?>
+}
