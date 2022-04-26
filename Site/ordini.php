babe27140b483891a50101ec001b6ba87dc74af9
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2022-04-26 11:41:24 +0200

    aggiunti ordini effettuati

---
+++ Site/ordini.php
@@ -0,0 +1,90 @@
+<?php
+    session_start();
+
+    use DB\Service;
+    require_once('backend/db.php');
+    require_once "graphics.php";
+
+    if(!isset($_SESSION["Nome"]))
+    {
+        header("Location: index.php");
+    }
+    else
+    {   
+        $paginaHTML = graphics::getPage("ordini_php.html");
+
+        // Accesso al database
+        $connessione = new Service();
+        $a = $connessione->openConnection();
+
+        $queryOrdini = $connessione->get_order_books($_SESSION["Codice_identificativo"]);
+
+        $tabellaOrdini = "";
+        
+        if ($queryOrdini->ok() && !$queryOrdini->is_empty())
+        {
+            $tabellaOrdini = "<table class='tabellaOrdini' title='Ordini Effettuati'>";
+            $tabellaOrdini .=   "<thead>";
+            $tabellaOrdini .=       "<tr>";
+            $tabellaOrdini .=           "<th scope='col'>Codice Ordine</th>";
+            $tabellaOrdini .=           "<th scope='col'>ISBN</th>";
+            $tabellaOrdini .=           "<th scope='col'>Titolo</th>";
+            $tabellaOrdini .=           "<th scope='col'>Quantit&agrave;</th>";
+            $tabellaOrdini .=           "<th scope='col'>Data Ordine</th>";
+            $tabellaOrdini .=           "<th scope='col'>Data Consegna</th>";
+            $tabellaOrdini .=           "<th scope='col' class='tdthDestra'>Totale</th>";
+            $tabellaOrdini .=           "</tr>";
+            $tabellaOrdini .=   "</thead>";
+
+            $tabellaOrdini .=   "<tbody>";
+
+            // Calcolo rowspan
+            $rowspans = array_count_values(array_column($queryOrdini->get_result(), 'codice_univoco'));
+
+            foreach($queryOrdini->get_result() as $ordine)
+            {
+                $rs = 0;
+                if(array_key_exists($ordine['codice_univoco'], $rowspans))
+                {
+                    $rs = $rowspans[$ordine['codice_univoco']];
+                    unset($rowspans[$ordine['codice_univoco']]);
+                }
+
+                $tabellaOrdini .= "<tr>";
+
+                if($rs > 0)
+                {
+                    $tabellaOrdini .=   "<td rowspan='" . $rs . "'>" . $ordine['codice_univoco'] . "</td>";
+                }
+                
+                $tabellaOrdini .=   "<td><a href='libro.php?isbn=" . $ordine['isbn'] . "'>" . $ordine['isbn'] . "</a></td>";
+                $tabellaOrdini .=   "<th scope='row'>" . $ordine['titolo'] . "</th>";
+                $tabellaOrdini .=   "<td>" . $ordine['quantita'] . "</td>";
+
+                if($rs > 0)
+                {
+                    $tabellaOrdini .=   "<td rowspan='" . $rs . "'>" . $ordine['data'] . "</td>";
+                    $tabellaOrdini .=   "<td rowspan='" . $rs . "'>" . $ordine['data_consegna'] . "</td>";
+                    $tabellaOrdini .=   "<td rowspan='" . $rs . "' class='tdthDestra'>&euro;" . $ordine['totale'] . "</td>";
+                }
+                $tabellaOrdini .= "</tr>";
+            }
+            $tabellaOrdini .=   "</tbody>";
+
+            // tfoot?
+
+            $tabellaOrdini .= "</table>";
+        }
+        else
+        {
+            // questo utente non ha ordini
+        }
+
+        $paginaHTML = str_replace("</tabellaOrdini>", $tabellaOrdini, $paginaHTML);
+
+        $connessione->closeConnection();
+        // -------------------
+
+        echo $paginaHTML;
+    }
+?>
