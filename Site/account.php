4c24df78c2c7208ab0612b81609a800017ece25d
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2022-04-26 11:40:14 +0200

    Add files via upload

---
+++ Site/account.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["Nome"]))
+    {
+        header("Location: index.php");
+    }
+    else
+    {
+        require_once "graphics.php";
+        
+        $paginaHTML = graphics::getPage("account_php.html");
+
+        // Accesso al database
+
+        // -------------------
+
+        echo $paginaHTML;
+    }
+?>
