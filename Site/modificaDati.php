a5e9dad0b75905d174fcb89b70390c2a88eced10
casYy <51257666+casYy@users.noreply.github.com>
2022-06-21 12:15:03 +0200

    fix dati login

--- Site/modificaDati.php
+++ Site/modificaDati.php
@@ -4,17 +4,19 @@
 
     use DB\Service;
 
-    if(!isset($_SESSION["Nome"])||!isset($_POST['username'])||!isset($_POST['username'])||!isset($_POST['password']))
+    if(!isset($_SESSION["Nome"]))
     {
         header("Location: index.php");
+    }
+    else if(!isset($_POST['username'])||!isset($_POST['email']))
+    {
+        header("Location: datilogin.php");
     }
     else
     {
         require_once('backend/db.php');
         $connessione = new Service();
         $a = $connessione->openConnection();
-
-        //print_r($_SESSION);
         
         $oldUsername = $_SESSION['Username'];
         $oldMail = $_SESSION['Email'];
@@ -25,7 +27,6 @@
             "email" => $oldMail,
         );
 
-        print_r($oldArray);
 
         $newUsername = $_POST['username'];
         $newMail = $_POST['email'];
@@ -35,8 +36,13 @@
             "email" => $newMail,
         );
 
+        print_r($oldArray);
+        print_r($newArray);
+
         $connessione->update_user_data($_SESSION['Codice_identificativo'],$oldArray,$newArray);
     
         $connessione->closeConnection();
+
+        header("Location:datilogin.php");
     }
 ?>
